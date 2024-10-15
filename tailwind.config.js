d4016f2da7a36bebaa1d1fb18ffa5fdb8f6ce436
Matheus Navarro <matheus.snavarr@outlook.com>
2024-10-15 14:37:28 +0200

    first

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        pri: '#334D2A',//'#004a35',
+        sec: '#d5e6b1',
+        thi: '#87441f',
+      },
+      padding: {
+        '100': '0px 10px',
+        's100': '0px 50px',
+        'm100': '0px 100px',
+        'l100': '0px 150px',
+        'x100': '0px 200px',
+      }
+    },
+  },
+  plugins: [],
+}
